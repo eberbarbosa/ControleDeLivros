965a2bcf510d0e498314ebb92f07938e9faa972a
Eber Barbosa <eberbarbosa255@gmail.com>
2025-06-01 17:20:51 -0300

    Projeto controle de livros no Spring Boot iniciado

---
+++ src/main/java/com/eber/controle_de_livros/ControleDeLivrosApplication.java
@@ -0,0 +1,19 @@
+package com.eber.controle_de_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeLivrosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ControleDeLivrosApplication.class, args);
+
+	}
+
+}
+
+
+
+
---
+++ src/test/java/com/eber/controle_de_livros/ControleDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eber.controle_de_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
